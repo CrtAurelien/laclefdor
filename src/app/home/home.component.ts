48772a18ddbaa7e9af3c64169230ef3946099f1b
victor <guerinonvictor@outlook.fr>
2022-07-13 12:00:56 +0200

    Horaire homePage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {BackgroundSwitchService} from "../shared/services/background-switch.service";
+import {FeaturesService} from "../shared/services/features.service";
 
 @Component({
   selector: 'app-home',
@@ -7,15 +8,16 @@ import {BackgroundSwitchService} from "../shared/services/background-switch.serv
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  featureResaIsOn = false;
   className = "blue"
 
-  constructor(private backgroundService:BackgroundSwitchService) { }
+  constructor(private backgroundService: BackgroundSwitchService, private featureService: FeaturesService) { }
 
   ngOnInit(): void {
-    this.backgroundService.setNomClasseDynamique(this.className)
+    this.backgroundService.setNomClasseDynamique(this.className);
     let burgerMenu = document.getElementById('navbarSupportedContent');
-    burgerMenu.classList.remove('show')
+    burgerMenu.classList.remove('show');
+    this.featureResaIsOn = this.featureService.featureIsOn('reservation');
   }
 
 }
